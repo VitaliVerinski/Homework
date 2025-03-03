10d953e743aaafe84ddfdc0f8310d80169632cec
Vitaliu Vertinski <Vitaliu.Vertinski@gmail.com>
2025-03-03 17:28:42 +0300

    lesson_6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,7 +17,7 @@ public class Main {
         dogBobik.run(200);
         dogBobik.swim(6);
 
-        // Выводим общее количество животных
+
         System.out.println("Всего животных: " + Animal.infoAllAnimals());
         System.out.println("Всего котов: " + Animal.infoAllCats());
         System.out.println("Всего собак: " + Animal.infoAllDogs());
@@ -25,10 +25,30 @@ public class Main {
         Bowl bowl = new Bowl(30);
         Cat.setBowl(bowl);
 
-
-        // Коты пытаются покушать
         catBoris.eat(10);
         catSnezok.eat(20);
         catBoris.eat(5);
+
+        Task2 circle = new Circle(5, "Красный", "Черный");
+        Task2 rectangle = new Rectangle(4, 6, "Зеленый", "Черный");
+        Task2 triangle = new Triangle(3, 4, 5, "Красный", "Синий");
+
+        System.out.println("Круг: ");
+        System.out.println("Цвет фона: " + circle.getFillColor());
+        System.out.println("Цвет границ: " + circle.getBorderColor());
+        System.out.println("Периметр: " + circle.calculatePerimeter());
+        System.out.println("Площадь: " + circle.calculateArea());
+
+        System.out.println("Прямоугольник: ");
+        System.out.println("Цвет фона: " + rectangle.getFillColor());
+        System.out.println("Цвет границ: " + rectangle.getBorderColor());
+        System.out.println("Периметр: " + rectangle.calculatePerimeter());
+        System.out.println("Площадь: " + rectangle.calculateArea());
+
+        System.out.println("Треугольник: ");
+        System.out.println("Цвет фона: " + triangle.getFillColor());
+        System.out.println("Цвет границ: " + triangle.getBorderColor());
+        System.out.println("Периметр: " + triangle.calculatePerimeter());
+        System.out.println("Площадь: " + triangle.calculateArea());
     }
-}
+    }
