904c3c21fed55150710bd45b2d80f0e52028d9d8
Vitaliu Vertinski <Vitaliu.Vertinski@gmail.com>
2025-03-09 23:25:51 +0300

    lesson_8

---
+++ src/main/java/org/example/Array.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Array  {
+    public static int totalSum(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Размер массива должен быть 4 на 4");
+        }
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+        return sum;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,46 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[][] Array1 = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        String[][] Array2 = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"}
+        };
+        String[][] Array3 = {
+                {"1", "2", "три", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        try {
+            int sum = Array.totalSum(Array1);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            int sum = Array.totalSum(Array2);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            int sum = Array.totalSum(Array3);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            String[][] array = new String[2][2];
+            System.out.println(array[3][3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка: " + e.getMessage());
         }
     }
 }
