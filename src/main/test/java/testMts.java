e11e95b82ab3a460fb10a164da5c3eab83822b16
Vitaliu Vertinski <Vitaliu.Vertinski@gmail.com>
2025-04-01 15:01:29 +0300

    lesson_14

--- src/main/test/java/testMts.java
+++ src/main/test/java/testMts.java
@@ -3,13 +3,16 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class testMts {
-    private WebDriver driver;
+    public WebDriver driver;
 
     @Before
     public void setUp() {
@@ -18,18 +21,17 @@ public class testMts {
     }
 
     @Test
-    public void testOnlineReplenishmentBlockTitle() {
+    public   void testOnlineReplenishmentBlockTitle() {
         driver.get("http://mts.by");
-        Assertions.assertEquals("Visa", driver.findElement(By.id("pay__partners")));
-        driver.findElements(By.name("hgsfhdfghdfghdfghvsfg sfgsfgy"));
-       // Assert.assertNotNull("Блок не найден", blockTitle);
-       // Assert.assertEquals("Онлайн пополнение без комиссии", blockTitle.getText());
+        WebElement element = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/h2"));
+        System.out.println(element.getText());
     }
     @Test
     public void testPaymentSystemLogos() {
         driver.get("http://mts.by");
-              driver.findElements(By.className("pay__partners"));
-            Assert.assertNotNull("Логотип не найден");
+        WebElement element = driver.findElement(By.className("pay__partners"));
+        System.out.println(element.getText());
+        Assert.assertNotNull("Логотип не найден");
     }
     @Test
     public void testMoreAboutServiceLink() {
@@ -42,14 +44,13 @@ public class testMts {
     @Test
     public void testContinueButtonFunctionality() {
         driver.get("http://mts.by");
-       // WebElement servicesLink = driver.findElement(By.linkText("Услуги связи"));
-     //   servicesLink.click();
+        driver.findElements(By.linkText("Продолжить"));
         WebElement phoneNumberField = driver.findElement(By.id("connection-phone"));
         phoneNumberField.sendKeys("297777777");
-        WebElement continueButton = driver.findElement(By.id("button button__default "));
+        WebElement pay = driver.findElement(By.id("connection-sum"));
+        pay.sendKeys("10");
+        WebElement continueButton = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button"));
         continueButton.click();
-        String expectedUrl = "https://www.mts.by/";
-        Assert.assertEquals("URL не соответствует ожидаемому", expectedUrl, driver.getCurrentUrl());
     }
 
     @After
